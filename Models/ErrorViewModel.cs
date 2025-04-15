1f6a76cb51493e1189df178661d0a38b04ffa579
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-04-15 23:02:20 +0200

    initial commit with project template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FileBasedMediaEngine.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
