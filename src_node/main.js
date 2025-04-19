926097db41f4a932bd2e5427c4878ccae833116a
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-04-19 19:42:47 +0200

    fix: update layout with vite path and fetch more components

--- src_node/main.js
+++ src_node/main.js
@@ -1,1 +1,3 @@
 import "markup-refine-lib/dist/markup-refine-lib.css";
+import "markup-refine-lib/dist/markup-refine-lib-web-components.css";
+import "markup-refine-lib/dist/markup-refine-lib-web-components.js";
